eb4b06b6601840f81cd833f2720cc71949d3baad
272002 <108273339+272002@users.noreply.github.com>
2022-07-15 17:54:25 -0700

    commit

---
+++ list.py
@@ -0,0 +1,33 @@
+from pickle import TRUE
+from re import S
+
+
+# lst = [1,2,4,5,'hello',['q','w']]
+# print(lst)
+# print(lst[0])
+# print(lst[-1])
+# print(lst[-2][0])
+
+# lst[0] = 100
+# print(lst)
+# lst[4][0]='v'
+# print(lst)
+
+# random_number_list = [12,34,1,23,66,56]
+# random_number_list.sort()
+# print(random_number_list)
+# random_number_list.sort(reverse=True)
+# print(random_number_list)
+# random_number_list.append(100)
+# print(random_number_list)
+
+# a = [1,2,3]
+# b = [4,5,6]
+# print(a + b)
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)
+
+a = [1,2,3,4,'Hello World',5,6,7,8]
+print(a[1:4:2])
