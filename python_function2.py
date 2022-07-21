2eaff84a125b589423b5753ffb0633c209a163e0
272002 <108273339+272002@users.noreply.github.com>
2022-07-20 18:26:44 -0700

    commit

---
+++ python_function2.py
@@ -0,0 +1,43 @@
+
+# def lt():
+#     lst = [1,2,3]
+#     return lst
+
+# a = lt()
+# print(a)
+
+
+# def check(st):
+#     return st.isupper()
+        
+# st = input("Enter string ")
+# print(check(st))
+
+
+# def total(st):
+#     return sum(map(ord, st))
+# st = input("Enter string ")
+# print(total(st))
+
+
+# def generate_password(username,password):
+#     a = username[:4:1]
+#     b = password[-4::]
+#     return a+b
+
+# print(generate_password('amanjain','1234567'))
+
+# def count_(st):
+#     count1 = 0
+#     for i in range(1,len(st)):
+#         if (ord(st[i]) == ord(st[i-1])+1):
+#             count1 = count1+1
+        
+#     return count1
+
+# print(count_('aabab'))
+
+
+
+
+
