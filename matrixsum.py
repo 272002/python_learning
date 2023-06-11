f5b5b52ff1bdb023005dd2a1e1c36a2a2cb07c2b
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-06-11 16:04:37 +0530

    commit

---
+++ matrixsum.py
@@ -0,0 +1,21 @@
+l1 = [[1,2,3],[4,5,6],[7,8,9]]
+l2 = [[21,22,23],[24,25,26],[27,28,29]]
+l3=[[0,0,0],[0,0,0],[0,0,0]]
+# for i in range(0,len(l1)):
+#     for j in range(0,len(l1[0])):
+#         l3[i][j] = l1[i][j] + l2[i][j]
+
+# print(l3)
+
+
+
+# r = int(input())
+# c = int(input())
+# l1 =[]
+# for i in range(0,r):
+#     l2 = []
+#     for j in range(0,c):
+#         s = int(input())
+#         l2.append(s)
+#     l1.append(l2)
+# print(l1)
